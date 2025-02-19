fe7be5ea07e24df6d543c64d071d8a8e93873b06
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-02-19 18:00:05 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("Name: [cristine schei quilay]")
+print("Course: [BSIT-3Q]")
+print("Interests in programming: [developer]")
+Name: [cristine schei quilay]
+Course: [bsit-3Q]
+Interests in programming: [developer]
